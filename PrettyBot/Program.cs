6adbae6fcddc5b8aa1412e552d9e4b604848b74f
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2019-10-29 20:18:18 +0300

    Initial commit

---
+++ PrettyBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrettyBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
