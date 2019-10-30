731cfad6c94854f82737929d05dbc3c39a468dd9
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2019-10-30 06:03:51 +0300

    Implemented search by words

--- PrettyBot/Program.cs
+++ PrettyBot/Program.cs
@@ -13,7 +13,7 @@ namespace PrettyBot
         private const string DefaultLang = "ru";
         private const string NotFoundAnswer = @"К сожалению, не могу ничего ответить на это ¯\_(ツ)_/¯";
         
-        private static Dictionary<string, LangData> _db;
+        private static Dictionary<HashSet<string>, LangData> _db;
 
         private static string _lang = DefaultLang;
         
@@ -30,27 +30,41 @@ namespace PrettyBot
 
             var data = File.ReadAllText("database.json");
             _db = JsonConvert.DeserializeObject<Dictionary<string, LangData>>(data)
-                .ToDictionary(kv => kv.Key.ToLower(), kv => kv.Value);
+                .ToDictionary(kv => ToWords(kv.Key), kv => kv.Value);
 
+//            while (true)
+//            {
+//                var updates = bot.GetUpdates();
+//                
+//                foreach (var update in updates)
+//                {
+//                    var answer = GetAnswer(update.Message.Text, _lang);
+//                    bot.SendMessage(update.Message.Chat.Id, answer);
+//                }
+//            }
             while (true)
             {
-                var updates = bot.GetUpdates();
-                
-                foreach (var update in updates)
-                {
-                    var answer = GetAnswer(update.Message.Text.ToLower(), _lang);
-                    bot.SendMessage(update.Message.Chat.Id, answer);
-                }
+                var req = Console.ReadLine();
+                var answer = GetAnswer(req, _lang);
+                Console.WriteLine(answer);
             }
+        }
+
+        static HashSet<string> ToWords(string sentence)
+        {
+            return sentence.Split(new char[0], StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => s.ToLower()).ToHashSet();
         }
 
         static string GetAnswer(string question, string lang = DefaultLang)
         {
             var answers = new List<string>();
+
+            var questionWords = ToWords(question);
             
             foreach (var (q, a) in _db)
             {
-                if (!question.Contains(q)) continue;
+                if (!q.IsSubsetOf(questionWords)) continue;
                 
                 // try to find answer for specified language
                 if (!a.TryGetValue(lang, out var answer))
